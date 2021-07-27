f0b46bc2d42091a94b5ea5dc15b265bacbf7e1cf
Simon Morris <simonjmorris213@gmail.com>
2021-07-27 02:48:59 -0700

    stuff from mosh videos

--- src/com/company/FirstProject.java
+++ src/com/company/FirstProject.java
@@ -3,6 +3,6 @@ package com.company;
 public class FirstProject {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        System.out.println("Hello, World!");
     }
 }
